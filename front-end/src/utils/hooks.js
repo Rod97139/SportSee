89a886c1f687df3072e6f61a104b517f9a5da1f9
Rod97139 <fayaflame@gmail.com>
2024-05-02 02:07:12 +0200

    custom useFetch with axios

---
+++ front-end/src/utils/hooks.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+export const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (!url) return;
+        setIsLoading(true);
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(url);
+                const data = await response?.data;
+                setData(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchData();
+    }, [url]);
+    return { isLoading, data, error };
+}
